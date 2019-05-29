d9d5629dcc0e6d467f749cd6cc86ae999a95c8d1
Philipp Schulz <hadeweka@outlook.de>
2019-05-29 23:39:21 +0200

    Restructuring of scripts

--- Main.cpp
+++ Main.cpp
@@ -3,6 +3,8 @@
 int main() {
 
 	auto mrb = mrb_open();
+
+	//! Load wrapped classes and Ruby scripts
 	
 	setup_ruby_class_resource_manager(mrb);
 
@@ -18,6 +20,33 @@ int main() {
 	setup_ruby_class_window(mrb);
 
 	MRB_LOAD_SCRIPT(mrb, Scene);
+
+	MRB_LOAD_SCRIPT(mrb, MainLoop);
+
+	//! Load potential mods
+
+	auto current_path = std::filesystem::current_path();
+	auto mod_path = current_path / "scripts" / "mods";
+
+	if (!std::filesystem::exists(mod_path)) {
+
+		std::clog << "No mod folder found." << std::endl;
+
+	} else {
+
+		std::clog << "Mod folder found. Loading mod files..." << std::endl;
+
+		for (const auto& entry : std::filesystem::directory_iterator(mod_path)) {
+
+			std::cout << "Loading file: " << entry << std::endl;
+			const std::string str = entry.path().string();
+			MrbWrap::execute_script_file(mrb, str);
+
+		}
+
+	}
+
+	//! Start main script with the game loop
 
 	MRB_LOAD_SCRIPT(mrb, Main);
 
--- Main.h
+++ Main.h
@@ -2,12 +2,19 @@
 
 #include <SFML/Graphics.hpp>
 
+#include <filesystem>
+#include <iostream>
+
 #include "Helper.h"
 
 #ifdef NDEBUG
 #include "compiled_scripts/Scene.h"
+#include "compiled_scripts/Scenes.h"
 #include "compiled_scripts/GlobalContainer.h"
 #include "compiled_scripts/Entity.h"
+#include "compiled_scripts/Entities.h"
+#include "compiled_scripts/MainLoop.h"
+#include "compiled_scripts/Main.h"
 #endif
 
 #include "ResourceManager.h"
